d84d7197ebb5f43945c0434dbdfb31e4ab9609b1
Jonas <wjlester77@gmail.com>
2024-03-20 12:03:06 +0800

    init 002

--- src/static/tailwind.config.js
+++ src/static/tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["../templates/**/*.html"],
   theme: {
-    extend: {},
+    screens: {
+      md: '1120px',
+      lg: '1120px',
+      xl: '1120px',
+      '2xl': '1120px'
+    },
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        primary: '#E84545',
+        secondary: '#903749',
+        teritary: '#522546',
+        'blog-dark': '#2B2E4A'
+      }
+    },
   },
   plugins: [],
 }
