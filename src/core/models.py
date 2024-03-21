ff54be3e1b75335625d30e50772d480a9235eb1a
Jonas <wjlester77@gmail.com>
2024-03-21 08:20:02 +0800

    添加分類與博文的model

--- src/core/models.py
+++ src/core/models.py
@@ -24,3 +24,52 @@ class Profile(models.Model):
     class Meta:
         verbose_name = '個人資料'
         verbose_name_plural = '個人資料'
+
+
+class Category(models.Model):
+    """ 博文分類 """
+    slug = models.SlugField('地址縮寫', max_length=120)
+    name = models.CharField('名稱', max_length=50)
+    description = models.CharField('描述', max_length=250, blank=True)
+    order = models.IntegerField('排序', default=0)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('-order', 'id')
+        verbose_name = '博文分類'
+        verbose_name_plural = '博文分類'
+
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', '草稿'),
+        ('published', '發佈')
+    )
+    title = models.CharField('標題', max_length=250)
+    slug = models.SlugField('地址縮寫', max_length=120, unique_for_date='created')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts', verbose_name='分類')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts', verbose_name='作者')
+    img = models.ImageField('圖片', upload_to='uploads', blank=True, null=True)
+    summary = models.CharField('摘要', max_length=250, blank=True)
+    body = models.TextField('正文')
+
+    tags = models.CharField('標簽', max_length=200)
+
+    views = models.IntegerField('瀏覽數', default=0)
+    flag = models.BooleanField('標記', default=False)
+    status = models.CharField('狀態', max_length=120, choices=STATUS_CHOICES, default='draft')
+
+    updated = models.DateTimeField('更新時間', auto_now=True)
+    created = models.DateTimeField('創建時間', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+    class Meta:
+        verbose_name = '博文日誌'
+        verbose_name_plural = '博文日誌'
+        ordering = ('-created',)
