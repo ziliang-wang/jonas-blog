8a6bf1c564c571ff7fabcdd4d5379ab7c5061d4e
Jonas <wjlester77@gmail.com>
2024-03-23 13:24:21 +0800

    search highlight

--- src/core/templatetags/core_extras.py
+++ src/core/templatetags/core_extras.py
@@ -9,6 +9,12 @@ def int_to_str(value):
     return str(value)
 
 
+@register.filter(name='highlight')
+def highlight_title(text, search):
+    if search:
+        text = text.replace(search, f'<span class="bg-secondary text-white">{ search }</span>')
+    return text
+
 # hot tags
 @register.inclusion_tag('core/tags.html')
 def display_tags():
