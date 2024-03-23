c9abc797900addd79749db680e7c9bfdfab66f79
Jonas <wjlester77@gmail.com>
2024-03-23 10:10:37 +0800

    共享數據邏輯3

--- src/core/context_processors.py
+++ src/core/context_processors.py
@@ -7,7 +7,6 @@ def post_processor(request):
     return {
         # 'CATEGORIES': Category.objects.all(),
         'CATEGORIES': Category.objects.values('id', 'name').annotate(Count('posts')),
-        'TAGS': ...,
         'LATEST_POSTS': Post.published.all()[:5],
         'RECOMMEND_POSTS': Post.published.filter(flag=True).all()[:5]
     }
--- src/core/templatetags/core_extras.py
+++ src/core/templatetags/core_extras.py
@@ -1,7 +1,19 @@
 from django import template
+from taggit.models import Tag
 
 register = template.Library()
+
 
 @register.filter(name='to_str')
 def int_to_str(value):
     return str(value)
+
+
+# hot tags
+@register.inclusion_tag('core/tags.html')
+def display_tags():
+    tags = Tag.objects.all()
+
+    return {
+        'tags': tags
+    }
